9cde4dcffb035daa6ff7b1df312c6134a7da8143
cmoutenet <MatMout120709!>
2018-06-03 21:39:58 +0200

    Try to load bad sportIdent format: line return in the name

--- plugins/fr.cm.scorexpress.core/src/main/java/fr/cm/scorexpress/data/UserChronosLoader.java
+++ plugins/fr.cm.scorexpress.core/src/main/java/fr/cm/scorexpress/data/UserChronosLoader.java
@@ -1,14 +1,14 @@
 package fr.cm.scorexpress.data;
 
-import java.io.*;
-import java.util.*;
-import java.util.regex.Pattern;
-
 import fr.cm.scorexpress.core.model.*;
 import fr.cm.scorexpress.core.model.impl.Date2;
 import fr.cm.scorexpress.core.model.impl.DateFactory;
 import fr.cm.scorexpress.core.model.impl.DateUtils;
 import fr.cm.scorexpress.core.model.impl.ObjStep;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static fr.cm.scorexpress.core.model.ObjDossard.VAR_DOSSARD_CATEGORIE;
@@ -32,19 +32,18 @@ import static fr.cm.scorexpress.core.model.ObjDossard.VAR_DOSSARD_CATEGORIE;
  */
 
 public class UserChronosLoader {
-    private final Collection<ObjUserChronos> userChronosList = new ArrayList<ObjUserChronos>();
-    private final Collection<String>         removedDossards = new ArrayList<String>();
-
-    private static final String                  SEPARATEUR        = ";";
-    private final        String                  fileName;
-    public static final  String                  VAR_NUM           = "N° dép.";
-    public static final  String                  VAR_NOM           = "Nom";
-    public static final  String                  VAR_PRENOM        = "Prénom";
-    public static final  String                  VAR_SEXE          = "S";
-    public static final  String                  VAR_PUCE          = "Puce";
-    public static final  String                  VAR_CSV_CATEGORIE = "Long";
-    public static final  String                  VAR_VILLE         = "Ville";
-    static final         HashMap<String, String> instances         = new HashMap<String, String>();
+    public static final  String                     VAR_NUM           = "N° dép.";
+    public static final  String                     VAR_NOM           = "Nom";
+    public static final  String                     VAR_PRENOM        = "Prénom";
+    public static final  String                     VAR_SEXE          = "S";
+    public static final  String                     VAR_PUCE          = "Puce";
+    public static final  String                     VAR_CSV_CATEGORIE = "Long";
+    public static final  String                     VAR_VILLE         = "Ville";
+    static final         HashMap<String, String>    instances         = new HashMap<String, String>();
+    private static final String                     SEPARATEUR        = ";";
+    private final        Collection<ObjUserChronos> userChronosList   = new ArrayList<ObjUserChronos>();
+    private final        Collection<String>         removedDossards   = new ArrayList<String>();
+    private final        String                     fileName;
 
     private UserChronosLoader(final String fileName) {
         this.fileName = fileName;
@@ -53,6 +52,36 @@ public class UserChronosLoader {
     public static UserChronosLoader createUserChrono(final String fileName) {
         instances.put(fileName, fileName);
         return new UserChronosLoader(fileName);
+    }
+
+    static String getLastFilename(final String numEtape) {
+        return instances.get(numEtape);
+    }
+
+    protected static Date2 getTime(final Date2 oldTime, final Date2 time) {
+        if (oldTime != null && time != null && !time.isNull()) {
+            while (!oldTime.before(time)) {
+                DateUtils.upTime(time, DateFactory.createDate("12:00:00"));
+            }
+            return time;
+        }
+        return oldTime;
+    }
+
+    public static boolean matchesStepCategoryFilter(final ObjStep etape, final String category) {
+        return category == null || etape.getCategoryFilter() == null || etape.getCategoryFilter().isEmpty() || Pattern
+                .compile("\\b" + etape.getCategoryFilter()).matcher(category).find();
+    }
+
+    private static void mergeBorne(ObjChrono ref, ObjChrono newChrono) {
+        if (newChrono != null) {
+            if (DateUtils.compare(ref, newChrono) < 0) {
+                ref.setTemps(newChrono.getTemps());
+            }
+            if (newChrono.isNull()) {
+                ref.getTemps().setNull();
+            }
+        }
     }
 
     private String getNext(final String ligne) {
@@ -75,10 +104,6 @@ public class UserChronosLoader {
         return ligne.indexOf(SEPARATEUR, 0) != -1;
     }
 
-    static String getLastFilename(final String numEtape) {
-        return instances.get(numEtape);
-    }
-
     public synchronized void loadFile(final ObjConfig config) {
         try {
             System.out.println("Chargement de " + fileName);
@@ -91,68 +116,86 @@ public class UserChronosLoader {
             // fichier
             final List<Integer> colonneInfos = newArrayList();
             final List<String>  colonneName  = newArrayList();
+            String              previousLine = "";
             for (int nrLigneCSV = 0; (ligne = dis.readLine()) != null; nrLigneCSV++) {
                 if (nrLigneCSV > 0) { // Titre
                     final ObjUserChronos       userChronos = new ObjUserChronos("0");
                     final ArrayList<ObjChrono> chronos     = new ArrayList<ObjChrono>();
-                    userChronosList.add(userChronos);
-                    String          puce    = null;
-                    final ObjChrono arrivee = new ObjChronoArrivee(DateFactory.createDate(0));
-                    for (int arg = 0; hasMore(ligne); arg++) {
-                        String val = getNext(ligne);
-                        ligne = nextSeparator(ligne);
-                        // System.out.println("" + val);
-                        final int indexCol = colonneInfos.indexOf(new Integer(arg));
-                        if (indexCol != -1 && indexCol < colonneName.size()) {
-                            if (!val.isEmpty() && val.charAt(0) == '"') {
-                                if (val.length() == 1) {
-                                    val = "";
-                                } else {
-                                    val = val.substring(1, val.length() - 1);
+                    String                     puce        = null;
+                    final ObjChrono            arrivee     = new ObjChronoArrivee(DateFactory.createDate(0));
+                    int                        arg         = 0;
+                    final String               newLine     = previousLine + ligne;
+                    if (previousLine.length() > 0) {
+                        System.err.println("Catching bad format:\n" + newLine);
+                    }
+                    ligne = newLine;
+                    previousLine = "";
+                    try {
+                        for (; hasMore(ligne); arg++) {
+                            String val = getNext(ligne);
+                            ligne = nextSeparator(ligne);
+                            // System.out.println("" + val);
+                            final int indexCol = colonneInfos.indexOf(new Integer(arg));
+                            if (indexCol != -1 && indexCol < colonneName.size()) {
+                                if (!val.isEmpty() && val.charAt(0) == '"') {
+                                    if (val.length() == 1) {
+                                        val = "";
+                                    } else {
+                                        val = val.substring(1, val.length() - 1);
+                                    }
+                                }
+                                final String property = colonneName.get(indexCol);
+                                userChronos.setInfo(property, val);
+                            }
+                            if (arg == 0) {
+                                Integer.valueOf(val);
+                                userChronos.setDossard(val);
+                            } else if (arg == 1) {
+                                puce = val;
+                                userChronos.setPuce(puce);
+                            } else if (arg == 4) {
+                                if (!val.isEmpty()) {
+                                    userChronos.setInfoDiverse("MULTI_ID", val);
+                                }
+                            } else if (arg == 9) {
+                                chronos.add(new ObjChronoDepart(DateFactory.createDate("0:00:00")));
+                            } else if (arg == 11) {
+                                arrivee.setTemps(DateFactory.createDate(val));
+                                userChronos.setPuce(puce);
+                                chronos.add(arrivee);
+                            } else if (arg >= 46) {
+                                final String tempsBaliseStr = getNext(ligne);
+                                if (hasMore(ligne)) {
+                                    ligne = nextSeparator(ligne);
+                                }
+                                arg++;
+                                final Date2 time = DateFactory.createDate(tempsBaliseStr);
+                                if (time.getTime() < 0) {
+                                    throw new RuntimeException(
+                                            "Negative time for num" + userChronos.getDossard() + ": " + time);
+                                } else if (!time.isNull()) {
+                                    final ObjChrono chrono = new ObjChrono(val);
+                                    chrono.setTemps(time);
+                                    chronos.add(chrono);
                                 }
                             }
-                            final String property = colonneName.get(indexCol);
-                            userChronos.setInfo(property, val);
                         }
-                        if (arg == 0) {
-                            if (val.isEmpty()) {
-                                System.out.println("Number exception ligne " + nrLigneCSV);
-                            } else {
-                                userChronos.setDossard(val);
-                            }
-                        } else if (arg == 1) {
-                            puce = val;
-                            userChronos.setPuce(puce);
-                        } else if (arg == 4) {
-                            if (!val.isEmpty()) {
-                                userChronos.setInfoDiverse("MULTI_ID", val);
-                            }
-                        } else if (arg == 9) {
-                            chronos.add(new ObjChronoDepart(DateFactory.createDate("0:00:00")));
-                        } else if (arg == 11) {
-                            arrivee.setTemps(DateFactory.createDate(val));
-                            userChronos.setPuce(puce);
-                            chronos.add(arrivee);
-                        } else if (arg >= 46) {
-                            final String tempsBaliseStr = getNext(ligne);
-                            if (hasMore(ligne)) {
-                                ligne = nextSeparator(ligne);
-                            }
-                            arg++;
-                            final Date2 time = DateFactory.createDate(tempsBaliseStr);
-                            if (time.getTime() < 0) {
-                                System.err.println("Negative time for num" + userChronos.getDossard() + ": " + time);
-                            } else if (!time.isNull()) {
-                                final ObjChrono chrono = new ObjChrono(val);
-                                chrono.setTemps(time);
-                                chronos.add(chrono);
-                            }
+                        for (final ObjChrono chrono : chronos) {
+                            userChronos.addChrono(chrono);
                         }
+                        userChronos.sortChronos();
+                        if (arg > 40) {
+                            userChronosList.add(userChronos);
+                        } else {
+                            throw new BadSportIdentFormat(null, nrLigneCSV, "Not enough information: " + arg,
+                                                          userChronos.getDossard());
+                        }
+                    } catch (Throwable e) {
+                        if (arg < 40) {
+                            previousLine = newLine;
+                        }
+                        new BadSportIdentFormat(e, nrLigneCSV, "", userChronos.getDossard()).printStackTrace();
                     }
-                    for (final ObjChrono chrono : chronos) {
-                        userChronos.addChrono(chrono);
-                    }
-                    userChronos.sortChronos();
                 } else {
                     if (config != null) {
                         for (final ColTable colTable : config.getColTableAll()) {
@@ -177,16 +220,6 @@ public class UserChronosLoader {
             System.err.println(ex);
         }
         updateChronoList();
-    }
-
-    protected static Date2 getTime(final Date2 oldTime, final Date2 time) {
-        if (oldTime != null && time != null && !time.isNull()) {
-            while (!oldTime.before(time)) {
-                DateUtils.upTime(time, DateFactory.createDate("12:00:00"));
-            }
-            return time;
-        }
-        return oldTime;
     }
 
     public final synchronized void loadInfo(final ObjConfig config, final ObjStep etape) {
@@ -238,11 +271,6 @@ public class UserChronosLoader {
 
     }
 
-    public static boolean matchesStepCategoryFilter(final ObjStep etape, final String category) {
-        return category == null || etape.getCategoryFilter() == null || etape.getCategoryFilter().isEmpty() || Pattern
-                .compile("\\b" + etape.getCategoryFilter()).matcher(category).find();
-    }
-
     public Collection<ObjUserChronos> getInfoSportIdent() {
         return userChronosList;
     }
@@ -255,14 +283,19 @@ public class UserChronosLoader {
             String multiId = userChronos.getInfoStr(MULTI_ID);
             if (userChronos.getInfo(MULTI_ID) != null && !multiId.equals("\"\"")) {
                 if (chronosByMultiId.containsKey(multiId)) {
-                    final ObjUserChronos oldUserChronos = chronosByMultiId.remove(multiId);
-                    final ObjUserChronos mergeChronos   = mergeChronos(oldUserChronos, userChronos);
-                    mergeChronos.setDossard(multiId);
 
-                    removedDossards.add(oldUserChronos.getDossard());
-                    removedDossards.add(userChronos.getDossard());
-                    removedDossards.remove(mergeChronos.getDossard());
-                    chronosByMultiId.put(multiId, mergeChronos);
+                    try {
+                        final ObjUserChronos oldUserChronos = chronosByMultiId.remove(multiId);
+                        final ObjUserChronos mergeChronos   = mergeChronos(oldUserChronos, userChronos);
+                        mergeChronos.setDossard(multiId);
+
+                        removedDossards.add(oldUserChronos.getDossard());
+                        removedDossards.add(userChronos.getDossard());
+                        removedDossards.remove(mergeChronos.getDossard());
+                        chronosByMultiId.put(multiId, mergeChronos);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     userChronos.setDossard(multiId);
                     chronosByMultiId.put(multiId, userChronos);
@@ -327,15 +360,11 @@ public class UserChronosLoader {
 
         return ref;
     }
+}
 
-    private static void mergeBorne(ObjChrono ref, ObjChrono newChrono) {
-        if (newChrono != null) {
-            if (DateUtils.compare(ref, newChrono) < 0) {
-                ref.setTemps(newChrono.getTemps());
-            }
-            if (newChrono.isNull()) {
-                ref.getTemps().setNull();
-            }
-        }
+class BadSportIdentFormat extends Exception {
+
+    public BadSportIdentFormat(final Throwable e, final int line, final String cause, final String dossard) {
+        super(cause + " dossard=" + dossard + " ligne=" + line, e);
     }
 }
